dbb5182d6cab20c82e346b76df1e76d8ea735d84
Jonathan <jonathangager75@gmail.com>
2024-11-08 11:35:29 +0100

    Add Prix et langage en FR

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [NgStyle, NgClass, UpperCasePipe, DatePipe],
+  imports: [NgStyle, NgClass, UpperCasePipe, DatePipe, CurrencyPipe],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
@@ -15,6 +15,7 @@ export class FaceSnapComponent implements OnInit{
   
   snapButtonText!: string;
   userHasSnapped! : Boolean
+  myPrice : number = 336.75;
 
 
 ngOnInit() {
