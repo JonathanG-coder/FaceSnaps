5a3b77ab4889a5e1171368011b1b4965f26f04cb
Jonathan <jonathangager75@gmail.com>
2024-11-11 10:49:00 +0100

    Add injection service face-snaps

--- src/app/models/face-snap.ts
+++ src/app/models/face-snap.ts
@@ -3,10 +3,10 @@ export class FaceSnap {
     location?: string;
 
     constructor(public title: string,
-                public description: string,
-                public imageUrl: string,
-                public createdAt: Date,
-                public snaps: number) {}
+        public description: string,
+        public imageUrl: string,
+        public createdAt: Date,
+        public snaps: number) { }
 
     addSnap(): void {
         this.snaps++;
@@ -17,5 +17,10 @@ export class FaceSnap {
     }
     setLocation(location: string): void {
         this.location = location;
-      }
-  }
+    }
+
+    withLocation(location: string): FaceSnap {
+        this.setLocation(location);
+        return this;
+    }
+}
