8223863ca47a25a4655661bf06d64748b8aa02a3
Jonathan <jonathangager75@gmail.com>
2024-11-12 10:13:02 +0100

    Add component landing page pour les Routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
