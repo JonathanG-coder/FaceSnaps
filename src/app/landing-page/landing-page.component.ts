3985809ea3458d8fe7307644d3d94182f931fbd6
Jonathan <jonathangager75@gmail.com>
2024-11-14 15:52:11 +0100

    Add formulaire

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
-import { Router, RouterLink} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    RouterLink
+    FormsModule,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
 
-  constructor(private router: Router){}
+  userEmail!: string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
 
 
   onContinue() {
     this.router.navigateByUrl('facesnaps')
-}
+  }
+
+  onSubmitForm(form : NgForm): void {
+    console.log (form.value);
+  }
 
 }
